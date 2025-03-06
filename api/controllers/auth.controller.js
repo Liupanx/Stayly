ccf7660c111f4debd9e8ab0a33b74b464b39bc39
000CL000 <claireliu9712@gmail.com>
2025-03-05 23:43:30 -0800

    Added JWT Auth

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import prisma from '../lib/prisma.js';
 
 export const register = async (req, res) => {
@@ -24,7 +25,42 @@ export const register = async (req, res) => {
     }
 }
 
-export const login = (req, res) => {
+export const login = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: { username }
+        })
+        if (!user) {
+            res.status(401).json({ message: 'Invalid credentials!' });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            res.status(401).json({ message: 'Invalid credentials!' });
+        }
+        // generate token
+        // expires time
+        const age = 1000 * 60 * 60 * 24 * 7;
+        const token = jwt.sign(
+            {
+                id: user.id
+            },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: age }
+        );
+        res
+            .cookie('token', token, {
+                httpOnly: true,
+                maxAge: age,
+                // secure: true
+            })
+            .status(200)
+            .json({ message: 'Logged in successfully!' });
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to login!' });
+        console.log(error);
+    }
     console.log('login router works!');
 }
 
